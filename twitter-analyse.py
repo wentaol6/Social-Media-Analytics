3598d0ca817ae92a77dce812242c06dcf061e20c
wentaol6 <wentaol6@student.unimelb.edu.au>
2024-03-19 22:30:14 +1100

    sort before merge

--- twitter-analyse.py
+++ twitter-analyse.py
@@ -25,19 +25,7 @@ def find_largest_json_file(directory="."):
     largest_file = max(json_files, key=lambda file: os.path.getsize(os.path.join(directory, file)))#find the largest json file
     return largest_file
 
-largest_json_file = find_largest_json_file()
-print(largest_json_file)
-
-# TODO 优化合并策略
 # TODO 变量名 小驼峰， 函数名 大驼峰
-def merge_dicts(dict1, dict2):
-    for key, value in dict2.items():
-        if key in dict1:
-            dict1[key] += value
-        else:
-            dict1[key] = value
-    return dict1
-
 def get_params(tweet: json):
     data = tweet.get('doc').get('data')
     hour = data.get('created_at').split(':')[0]
@@ -46,7 +34,7 @@ def get_params(tweet: json):
     return hour, day, sentiment
 
 # TODO 需要找到目录下最大的json文件，
-file_path = './twitter-1mb.json' # TODO 找到所有json文件，选择最大的
+file_path = find_largest_json_file()
 
 total_bytes = os.path.getsize(file_path)
 each_bytes = total_bytes // size
@@ -85,25 +73,25 @@ with open(file_path, 'r', encoding='utf-8') as tweet_file:
         # TODO 改分组的逻辑
         current_position += len(tweet_str)
 
-dict_list_list = comm.gather([happy_hour_dict, happy_day_dict, active_hour_dict, active_day_dict], root=0)
+local_maxes = {
+    "happiest hour ever": max(happy_hour_dict.items(), key=lambda x: x[1]),
+    "happyiest day ever": max(happy_day_dict.items(), key=lambda x: x[1]),
+    "most active hour ever": max(active_hour_dict.items(), key=lambda x: x[1]),
+    "most active day ever": max(active_day_dict.items(), key=lambda x: x[1]),
+}
 
-# TODO 同理，数据结构重构，研究一下怎么设计数据结构更合理
-happy_hour_dict = {}
-happy_day_dict = {}
-active_hour_dict = {}
-active_day_dict = {}
+all_maxes = comm.gather(local_maxes, root=0)
 
 if rank == 0:
-    for dict_list in dict_list_list:
-        happy_hour_dict = merge_dicts(happy_hour_dict, dict_list[0])
-        happy_day_dict = merge_dicts(happy_day_dict, dict_list[1])
-        active_hour_dict = merge_dicts(active_hour_dict, dict_list[2])
-        active_day_dict = merge_dicts(active_day_dict, dict_list[3])
+    global_maxes = {category: ("", 0) for category in local_maxes.keys()}
 
-    print(list(dict(sorted(happy_hour_dict.items(), key=lambda item: item[1])).items())[-1])
-    print(list(dict(sorted(happy_day_dict.items(), key=lambda item: item[1])).items())[-1])
-    print(list(dict(sorted(active_hour_dict.items(), key=lambda item: item[1])).items())[-1])
-    print(list(dict(sorted(active_day_dict.items(), key=lambda item: item[1])).items())[-1])
+    for maxes in all_maxes:
+        for category, (key, value) in maxes.items():
+            if value > global_maxes[category][1]:
+                global_maxes[category] = (key, value)
+
+    for category, (key, value) in global_maxes.items():
+        print(f" {category}: {key}, {value}")
 
     finish_time = datetime.datetime.now()
     print(finish_time - begin_time)
